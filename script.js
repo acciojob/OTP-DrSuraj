fcba59444387e1e4fde4a84b322e4e8dcec48088
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-01-01 18:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function handleInput(input, event) {
+    const maxLength = parseInt(input.getAttribute("maxlength"));
+    const inputValue = input.value;
+
+    if (inputValue.length === maxLength) {
+        const nextInput = input.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    } else if (inputValue.length === 0 && event.inputType === "deleteContentBackward") {
+        const prevInput = input.previousElementSibling;
+        if (prevInput) {
+            prevInput.focus();
+        }
+    }
+}
+function handleInput(input, event) {
+    const maxLength = parseInt(input.getAttribute("maxlength"));
+    const inputValue = input.value;
+
+    if (inputValue.length === maxLength) {
+        const nextInput = input.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    } else if (inputValue.length === 0 && event.inputType === "deleteContentBackward") {
+        const prevInput = input.previousElementSibling;
+        if (prevInput) {
+            prevInput.focus();
+        }
+    }
+}
